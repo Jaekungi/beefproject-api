0afa8847cfb27afc7574e82e57dfaac7253f420f
Jaekungi <chayaphon.ma@gmail.com>
2022-06-03 17:07:46 +0700

    route-auth-user

---
+++ routes/userRoute.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const userController = require("../controllers/userController");
+const postController = require("../controllers/postController");
+
+const router = express.Router();
+
+router.get("/me", userController.getMe);
+router.patch(
+  "/",
+  upload.fields([{ name: "profilePic", maxCount: 1 }]),
+  userController.updateProfile
+);
+router.get("/posts", postController.getUserPost);
+
+module.exports = router;
