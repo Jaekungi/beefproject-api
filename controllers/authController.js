d4a00456d72b3c1343b782b37ef3c1e4dbb4d9a2
Jaekungi <chayaphon.ma@gmail.com>
2022-06-03 17:39:26 +0700

    controllers

---
+++ controllers/authController.js
@@ -0,0 +1,66 @@
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { Op } = require("sequelize");
+const createError = require("../utils/createError");
+const { User } = require("../models");
+
+const genToken = (payload) =>
+  jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
+exports.login = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({
+      where: {
+        username: username,
+      },
+    });
+
+    if (!user) {
+      createError("invalid credential", 400);
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      createError("invalid credential", 400);
+    }
+
+    const token = genToken({ id: user.id });
+    res.json({ token });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.register = async (req, res, next) => {
+  try {
+    const { username, password, confirmPassword } = req.body;
+
+    if (!username) {
+      createError("username is required", 400);
+    }
+
+    if (!password) {
+      createError("password is required", 400);
+    }
+
+    if (password !== confirmPassword) {
+      createError("password and confirm password did not match", 400);
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = await User.create({
+      username,
+      password: hashedPassword,
+    });
+
+    const token = genToken({ id: user.id });
+    res.status(201).json({ token });
+  } catch (err) {
+    next(err);
+  }
+};
---
+++ controllers/userController.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const { User } = require("../models");
+// const FriendService = require("../services/friendService");
+const cloudinary = require("../utils/cloudinary");
+const createError = require("../utils/createError");
+
+exports.getMe = async (req, res) => {
+  const user = JSON.parse(JSON.stringify(req.user));
+
+  res.json({ user });
+};
+
+exports.updateProfile = async (req, res, next) => {
+  try {
+    if (!req.files) {
+      createError("profilePic is required", 400);
+    }
+
+    const updateValue = {};
+    if (req.files.profilePic) {
+      const result = await cloudinary.upload(req.files.profilePic[0].path);
+      if (req.user.profilePic) {
+        const splited = req.user.profilePic.split("/");
+        const publicId = splited[splited.length - 1].split(".")[0];
+        await cloudinary.destroy(publicId);
+      }
+      updateValue.profilePic = result.secure_url;
+    }
+
+    await User.update(updateValue, { where: { id: req.user.id } });
+    res.json(updateValue);
+  } catch (err) {
+    next(err);
+  } finally {
+    if (req.files.profilePic) {
+      fs.unlinkSync(req.files.profilePic[0].path);
+    }
+  }
+};
